fad2e4fd2863f953544d65be233d0daa8aab648c
Maria Vintimilla <mariavintimilla@Marias-MacBook-Air.local>
2016-02-14 17:22:36 -0500

    added js

--- todo.js
+++ todo.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
   $(document).on("click", ".btn-danger", function(){
-    $(this).parent().parent().remove();
+    $(this).parent().remove();
   });
   $(".btn-primary").on("click", function(e){
     e.preventDefault();
+//prevents default action of an element from happening
     var newItem = $("#newItem").val().trim();
-    var newRow = $("<tr>");
-    var checkbox = $("<input>").attr("type", "checkbox").attr("class", "checkboxes").attr("data-state", "not-checked");
+//.val returns or sets the value attribute of selected element(s)
+//.trim removes all newlines, spaces and tabs from beg & end of string
+    var newRow = $("<tr></tr>");
+    var checkbox = $("<input>").attr("checkbox")
+//.attr gets the value of an attribute for 1st element in set of matched elemts or attrbts for every match element
     var deleteBtn = $("<button>").addClass("btn btn-danger btn-xs").append("Delete");
     var itemTd = $("<td>").addClass("word-td").append(newItem).append(deleteBtn).prepend(checkbox);
     newRow.append(itemTd);
@@ -15,7 +19,7 @@ $(document).ready(function() {
   });
   $(document).on("click", ".checkboxes", function(){
     var dataState = $(this).attr("data-state");
-    if(dataState === "not-checked"){
+    if(dataState === "nocheck"){
     $(this).hide(500);
     $(this).parent().attr("class", "str");
     $(this).attr("data-state", "checked");
